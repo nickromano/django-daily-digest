9a01d37464683a0320d0680a5546d5a4d4569d0b
Nick Romano <nick.r.romano@gmail.com>
2018-01-03 20:12:54 -0800

    Use built in staff login required decorator instead

--- daily_digest/views.py
+++ daily_digest/views.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 
 from .config import daily_digest_config
 from .utils import charts_data_for_config
 
 
-@login_required
+@staff_member_required
 def preview_daily_digest(request):
-    if not request.user.is_staff:
-        return HttpResponseRedirect("/admin/login/?next=" + request.path)
-
     context = {
         'title': daily_digest_config.title,
         'charts': charts_data_for_config()
