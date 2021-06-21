53756c73a192eab1e2075c74de7f4f950997d865
Nick Romano <nick.r.romano@gmail.com>
2021-06-21 13:13:36 -0700

    Fix test runner

--- daily_digest/urls.py
+++ daily_digest/urls.py
@@ -4,7 +4,7 @@ from .views import preview_daily_digest
 
 urlpatterns = [
     url(
-        r"^admin/daily-digest-preview/$",
+        r"admin/daily-digest-preview/$",
         preview_daily_digest,
         name="preview-daily-digest",
     ),
--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
     url(r"^", include("daily_digest.urls")),
+    url(r"^admin/", admin.site.urls),
 ]
